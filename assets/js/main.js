47e30f2e3eab9fe132d90267dd69bc0899e9602f
grehov084 <grehov084@gmail.com>
2024-12-09 00:53:27 +0700

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
-let bannerBottom, bannerBottomContent, counter;
+let bannerBottom, bannerBottomContent, counter, menuToggle, closeToggle, menu;
 
 bannerBottom = document.querySelector(".banner-item-bottom-text");
 bannerBottomContent = document.querySelector(".banner-item-bottom-text").innerHTML;
 counter = 0;
+menuToggle = document.querySelector(".site-header-menu-toggle");
+closeToggle = document.querySelector(".site-header__close");
+menu = document.querySelector(".site-header-info");
 
 while(bannerBottom.offsetWidth < window.innerWidth){
     if(counter == 0){
@@ -15,3 +18,10 @@ while(bannerBottom.offsetWidth < window.innerWidth){
     bannerBottom.innerHTML += bannerBottomContent;
 }
 bannerBottom.innerHTML += bannerBottom.innerHTML;
+
+menuToggle.addEventListener("click", ()=>{
+    menu.classList.add("show");
+});
+closeToggle.addEventListener("click", ()=>{
+    menu.classList.remove("show");
+});
