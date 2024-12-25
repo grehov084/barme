d8f0986820951fce589eb9d5b383e49be725e0d8
grehov084 <grehov084@gmail.com>
2024-12-25 23:34:59 +0700

    123

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,8 +1,9 @@
-let sizeItems, sliderPictures, puctureSrc, mainPicture;
+let sizeItems, sliderPictures, puctureSrc, mainPicture, propsItems;
 
 sizeItems = document.querySelectorAll(".card-params-size-item");
 sliderPictures = document.querySelectorAll(".card-pic-list-item__img");
 mainPicture = document.querySelector(".card-pic-main__img");
+propsItems = document.querySelectorAll(".card-info-item-activator");
 
 sizeItems.forEach((sizeItem)=>{
     sizeItem.addEventListener("click", ()=>{
@@ -19,5 +20,11 @@ sliderPictures.forEach((sliderPicture)=>{
         activePicture.classList.remove("active");
         sliderPicture.parentNode.classList.add("active");
         mainPicture.setAttribute("src", sliderPicture.getAttribute("src"));
+    });
+});
+
+propsItems.forEach((propsItem)=>{
+    propsItem.addEventListener("click", ()=>{
+        
     });
 });
