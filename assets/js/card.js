89c929dbaceec3e414b7971e6d56ccc2829bb8dc
grehov084 <grehov084@gmail.com>
2024-12-26 15:27:40 +0700

    1

--- assets/js/card.js
+++ assets/js/card.js
@@ -5,6 +5,60 @@ sliderPictures = document.querySelectorAll(".card-pic-list-item__img");
 mainPicture = document.querySelector(".card-pic-main__img");
 propsItems = document.querySelectorAll(".card-info-item-activator");
 
+
+function checkIndentation(currentElemContent){
+    let parentElem = currentElemContent.parentNode, indentation = 0;
+    if(parseInt(window.getComputedStyle(parentElem).getPropertyValue("padding-left")) != 0){
+       indentation += parseInt(window.getComputedStyle(parentElem).getPropertyValue("padding-left"));
+    }
+    if(parseInt(window.getComputedStyle(parentElem).getPropertyValue("padding-right")) != 0){
+       indentation += parseInt(window.getComputedStyle(parentElem).getPropertyValue("padding-right"));
+    }
+    if(parseInt(window.getComputedStyle(parentElem).getPropertyValue("padding-top")) != 0){
+       indentation += parseInt(window.getComputedStyle(parentElem).getPropertyValue("padding-top"));
+    }
+    if(parseInt(window.getComputedStyle(parentElem).getPropertyValue("padding-bottom")) != 0){
+       indentation += parseInt(window.getComputedStyle(parentElem).getPropertyValue("padding-bottom"));
+    } 
+    if(parseInt(window.getComputedStyle(parentElem).getPropertyValue("margin-left")) != 0){
+       indentation += parseInt(window.getComputedStyle(parentElem).getPropertyValue("margin-left"));
+    }
+    if(parseInt(window.getComputedStyle(parentElem).getPropertyValue("margin-right")) != 0){
+       indentation += parseInt(window.getComputedStyle(parentElem).getPropertyValue("margin-right"));
+    }
+    if(parseInt(window.getComputedStyle(parentElem).getPropertyValue("margin-top")) != 0){
+       indentation += parseInt(window.getComputedStyle(parentElem).getPropertyValue("margin-top"));
+    }
+    if(parseInt(window.getComputedStyle(parentElem).getPropertyValue("margin-bottom")) != 0){
+       indentation += parseInt(window.getComputedStyle(parentElem).getPropertyValue("margin-bottom"));
+    } 
+    if(parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("padding-left")) != 0){
+       indentation += parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("padding-left"));
+    }
+    if(parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("padding-right")) != 0){
+       indentation += parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("padding-right"));
+    }
+    if(parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("padding-top")) != 0){
+       indentation += parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("padding-top"));
+    }
+    if(parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("padding-bottom")) != 0){
+       indentation += parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("padding-bottom"));
+    } 
+    if(parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("margin-left")) != 0){
+       indentation += parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("margin-left"));
+    }
+    if(parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("margin-right")) != 0){
+       indentation += parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("margin-right"));
+    }
+    if(parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("margin-top")) != 0){
+       indentation += parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("margin-top"));
+    }
+    if(parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("margin-bottom")) != 0){
+       indentation += parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("margin-bottom"));
+    } 
+    return indentation;
+}
+
 sizeItems.forEach((sizeItem)=>{
     sizeItem.addEventListener("click", ()=>{
         let activeSize = document.querySelector(".card-params-size-item.active");
@@ -25,6 +79,28 @@ sliderPictures.forEach((sliderPicture)=>{
 
 propsItems.forEach((propsItem)=>{
     propsItem.addEventListener("click", ()=>{
-        
+        let propsContent, height;
+        height = 0;
+        propsContent = propsItem.parentNode.querySelector(".card-info-item-content");
+        if(!propsContent.classList.contains("show")){
+            for(let i = 0; i < propsContent.childElementCount; i++){
+                console.log(propsContent.children[i].offsetHeight);
+                height += propsContent.children[i].offsetHeight;
+                height += checkIndentation(propsContent.children[i]);
+                
+            }
+            propsContent.style.maxHeight = height + "px";
+            setTimeout(
+                ()=>{
+                    propsContent.classList.add("show");
+                }, 100
+            );
+            openCheck = 1;
+        }
+        else{
+            propsContent.style.maxHeight = 0;
+            propsContent.classList.remove("show");
+            openCheck = 0;
+        }
     });
 });
