4931670d0d96bd4cbb5952836a7753c8a575227e
grehov084 <grehov084@gmail.com>
2024-12-29 01:26:06 +0700

    1

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,11 +1,11 @@
-let sizeItems, sizePropsItems, sliderPictures, puctureSrc, mainPicture, propsItems;
+let sizeItems, careElems, sizePropsItems, sliderPictures, puctureSrc, mainPicture, propsItems;
 
 sizeItems = document.querySelectorAll(".card-params-size-item");
 sizePropsItems = document.querySelectorAll(".content-product-size-list-item");
 sliderPictures = document.querySelectorAll(".card-pic-list-item__img");
 mainPicture = document.querySelector(".card-pic-main__img");
 propsItems = document.querySelectorAll(".card-info-item-activator");
-
+careElems = document.querySelectorAll(".item-care-elem-pic");
 
 function checkIndentation(currentElemContent){
     let parentElem = currentElemContent.parentNode, indentation = 0;
@@ -58,6 +58,27 @@ function checkIndentation(currentElemContent){
        indentation += parseInt(window.getComputedStyle(currentElemContent).getPropertyValue("margin-bottom"));
     } 
     return indentation;
+}
+
+function fadeIn(el, timeout, display){
+    if(el.style.opacity == 0){
+        el.style.opacity = 0;
+        el.style.display = display || 'flex';;
+        el.style.transition = `opacity ${timeout}ms`;
+        setTimeout(() => {
+            el.style.opacity = 1;
+        }, timeout);
+    }
+}
+
+function fadeOut(el, timeout){
+    el.style.opacity = 1;
+    el.style.transition = `opacity ${timeout}ms`;
+    el.style.opacity = 0;
+    body.style.cssText = "";
+    setTimeout(() => {
+       el.style.display = 'none';
+    }, timeout); 
 }
 
 sizePropsItems.forEach((sizePropsItem)=>{
@@ -130,4 +151,33 @@ Fancybox.bind(".card-pic-main__img", {
         click: "close", 
         wheel: "slide" 
     } 
+});
+
+
+
+careElems.forEach((careElem)=>{
+    careElem.addEventListener("mouseover", function(e){
+        let careElemInfo;
+        if(e.target.tagName == "SVG"){
+            careElemInfo = careElem.parentNode.parentNode.querySelector(".item-care-elem__text");
+        }
+        else if(e.target.tagName == "PATH"){
+            careElemInfo = careElem.parentNode.parentNode.parentNode.querySelector(".item-care-elem__text");
+        }
+        else{
+            careElemInfo = careElem.parentNode.querySelector(".item-care-elem__text");
+        }
+        if(!careElemInfo.classList.contains("show")){
+            careElemInfo.classList.add("show");
+            fadeIn(careElemInfo, 200);
+        }
+    });
+    careElem.addEventListener("mouseout", function(e){
+        let careElemInfo;
+        careElemInfo = careElem.parentNode.querySelector(".item-care-elem__text");
+        if(e.relatedTarget.tagName != "svg" && e.relatedTarget.tagName != "path" && careElemInfo.classList.contains("show")){
+            careElemInfo.classList.remove("show");
+            fadeOut(careElemInfo, 100);
+        }
+    });
 });
